8d255535e089a7bff8ad0b5667216fc4cb2a445c
ztp <studyztp@gmail.com>
2022-08-11 13:18:30 -0700

    added print_this

---
+++ print_this/print_this_src.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define GEM5
+
+#ifdef GEM5
+#include <gem5/m5ops.h>
+#endif
+
+int main(int argc, char *argv[]) {
+#ifdef GEM5
+  m5_work_begin(0, 0);
+#endif
+    if(argc<=2) {
+        printf("please enter a string and an number\n ex. ['print this' 9]\n");
+        exit(1);
+     }  //otherwise continue on our merry way....
+    int arg2 = atoi(argv[2]);  
+    for(int i =0; i<arg2; i++){
+        printf("%i %s\n",i+1,argv[1]);
+    }
+#ifdef GEM5
+  m5_work_end(0, 0);
+#endif
+}
